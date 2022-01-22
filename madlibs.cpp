403217b5ce4ca36095acf48b76d4335e02969d79
khang-highline <baokhang@students.highline.edu>
2022-01-21 22:07:37 -0800

    Read line instead of single word.

--- madlibs.cpp
+++ madlibs.cpp
@@ -3,39 +3,37 @@
 
 int main()
 {
-	// usage
-	std::cout << "Please enter multiple words answer in snake case.\n\n";
 	// prompt
 	std::string name;
 	std::cout << "Enter a name: ";
-	std::cin >> name;
+	getline(std::cin, name);
 	std::string country;
-	std::cout << "Enter a country: ";
-	std::cin >> country;
+	std::cout << "Enter a location: ";
+	getline(std::cin, country);
 	std::string degree;
 	std::cout << "Enter a degree: ";
-	std::cin >> degree;
+	getline(std::cin, degree);
 	std::string major;
 	std::cout << "Enter a major: ";
-	std::cin >> major;
+	getline(std::cin, major);
 	std::string school;
 	std::cout << "Enter a school: ";
-	std::cin >> school;
+	getline(std::cin, school);
 	std::string title;
-	std::cout << "Enter a work title: ";
-	std::cin >> title;
+	std::cout << "Enter a job: ";
+	getline(std::cin, title);
 	std::string field;
 	std::cout << "Enter a work field: ";
-	std::cin >> field;
+	getline(std::cin, field);
 	std::string hobby1;
-	std::cout << "Enter a hobby (verb): ";
-	std::cin >> hobby1;
+	std::cout << "Enter hobby 1 (verb): ";
+	getline(std::cin, hobby1);
 	std::string hobby2;
-	std::cout << "Enter a hobby (verb): ";
-	std::cin >> hobby2;
+	std::cout << "Enter hobby 2 (verb): ";
+	getline(std::cin, hobby2);
 	std::string hobby3;
-	std::cout << "Enter a hobby (verb): ";
-	std::cin >> hobby3;
+	std::cout << "Enter hobby 3 (verb): ";
+	getline(std::cin, hobby3);
 	std::cout << "\n\n";
 	// output
 	std::cout << "My name is " << name << ". \n"
